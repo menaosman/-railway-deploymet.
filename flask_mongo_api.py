435b11bb2ad7a9223a414c377d8e49d9fff80b1c
Mena Mohamed <104127946+menaosman@users.noreply.github.com>
2025-05-09 06:03:49 +0300

    Create flask_mongo_api.py

---
+++ flask_mongo_api.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+from pymongo import MongoClient
+from datetime import datetime
+import os
+
+app = Flask(__name__)
+CORS(app)
+
+# MongoDB connection using Environment Variable
+mongo_uri = os.getenv("MONGO_URI", "mongodb+srv://biomedicalinformatics100:MyNewSecurePass%2123@cluster0.jilvfuv.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+client = MongoClient(mongo_uri, tls=True, tlsAllowInvalidCertificates=True)
+collection = client["sentiment_analysis"]["tweets"]
+
+@app.route('/fetch', methods=['GET'])
+def fetch_tweets():
+    try:
+        cursor = collection.find({}, {"_id": 0, "Text": 1, "Sentiment": 1, "Timestamp": 1})
+        tweets = list(cursor)
+
+        for tweet in tweets:
+            ts = tweet.get("Timestamp")
+            if isinstance(ts, datetime):
+                tweet["Timestamp"] = ts.strftime("%Y-%m-%d %H:%M:%S")
+            else:
+                tweet["Timestamp"] = "N/A"
+
+        return jsonify(tweets)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+if __name__ == '__main__':
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
